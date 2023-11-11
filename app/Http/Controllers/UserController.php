d2255e52e37e3ac7df4427bc385ef3677a3b53f6
ashrafcu <ahmashraf@gmail.com>
2023-11-11 19:05:57 +0600

    Final Changes

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function index(){
+        return "<h2>Hello, Laravel!</h2>";
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,6 +18,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/hello', function(){
-    return "Hello, Laravel!";
-});
+Route::get('/hello', [UserController::class,'index']);
